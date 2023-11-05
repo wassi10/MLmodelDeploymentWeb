924c1ddad834afa2889138c6a6039ad2eef42eb8
wassi10 <khadizawassi@gmail.com>
2023-11-05 18:32:56 +0600

    Basic Conf Done

--- MLmodelDeploymentWeb/settings.py
+++ MLmodelDeploymentWeb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'heartDiseasePrediction',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'MLmodelDeploymentWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- heartDiseasePrediction/views.py
+++ heartDiseasePrediction/views.py
@@ -1,3 +1,34 @@
+# from multiprocessing import context
 from django.shortcuts import render
+
+def Welcome(request):
+    return render(request, 'index.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def index(request):
+#     context={'a':1}
+#     return render(request, 'index.html')
 
 # Create your views here.
