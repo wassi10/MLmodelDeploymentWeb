a5e591a3affdad650435a1c83061b1e91caec140
wassi10 <khadizawassi@gmail.com>
2024-02-01 19:33:53 +0600

    signup backend

--- heartDiseasePrediction/views.py
+++ heartDiseasePrediction/views.py
@@ -1,5 +1,8 @@
 # from multiprocessing import context
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+
 
 def Welcome(request):
     return render(request, 'index.html')
@@ -28,8 +31,102 @@ def DoctorAndHospital(request):
     return render(request, 'doctorHospitalList.html')
 
 
+#For the user to resister or sign up.
+def SignUp(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        # Check if username already exists
+        if User.objects.filter(username=username).exists():
+            messages.info(request, 'Username is already taken')
+            return redirect('signup')
+        
+        # Check if email already exists
+        elif User.objects.filter(email=email).exists():
+            messages.info(request, 'Email is already registered')
+            return redirect('signup')
+        
+        # Check if passwords match
+        elif password1 != password2:
+            messages.info(request, 'Passwords do not match')
+            return redirect('signup')
+
+        else:
+            # Create the user if all validations pass
+            user = User.objects.create_user(username=username, email=email, password=password1)
+            user.save()
+            # messages.success(request, f'Account created for {username}. You can now log in!')
+            return redirect('signin')
+
+    return render(request, 'signup.html')
 
 
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def signup(request): #For the user to resister or sign up.
+
+    # if request.method == 'POST':
+    #     first_name = request.POST['first_name']
+    #     last_name = request.POST['last_name']
+    #     username = request.POST['username']
+    #     password = request.POST['password']
+    #     email = request.POST['email']
+
+        
+    #     if User.objects.filter(username=username).exists():
+    #         messages.info(request,'Username taken')
+    #         return redirect('signup')
+    #     elif User.objects.filter(email=email).exists():
+    #         messages.info(request,'Email taken')
+    #         return redirect('signup')
+    #     else:
+    #         user = User.objects.create_user(username=username, password=password,email=email,first_name = first_name,last_name=last_name)
+            
+    #         user.save()
+            
+    #         messages.success(request,f"User {username} created!")
+    #         return redirect('signin')
+    #     #return redirect('/')
+    # else:   
+#         return render(request,'signup.html')
+
+
+# def signout(request): # In order to logout from the website
+#     auth.logout(request)
+#     return redirect('/')
 
 
 
