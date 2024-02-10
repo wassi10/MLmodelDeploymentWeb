e7aa31732ec771b8b253c6d72ceca51b8152f48d
wassi10 <khadizawassi@gmail.com>
2024-02-10 19:14:47 +0600

    forgot pass ui

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,35 @@
 // <!-- === Sticky Navbar === -->
 
-    window.addEventListener('scroll', function () {
-      var header = document.querySelector('.header')
-      header.classList.toggle('sticky', window.scrollY > 0)
-    })
- 
+window.addEventListener('scroll', function () {
+  var header = document.querySelector('.header')
+  header.classList.toggle('sticky', window.scrollY > 0)
+})
+
+// forgot pass eyeon, off
+var pass
+function resetpass() {
+  if (pass == 1) {
+    document.getElementById('password1').type = 'password';
+    document.getElementById('pass-icon').src = 'static/img/eyeon.png';
+    pass = 0;
+  }
+  else {
+    document.getElementById('password1').type = 'text';
+    document.getElementById('pass-icon').src = 'static/img/eyeoff.png';
+    pass = 1;
+  }
+}
+
+var pass1
+function resetpass1() {
+  if (pass1 == 1) {
+    document.getElementById('password2').type = 'password';
+    document.getElementById('pass-icon1').src = 'static/img/eyeon.png';
+    pass1 = 0;
+  }
+  else {
+    document.getElementById('password2').type = 'text';
+    document.getElementById('pass-icon1').src = 'static/img/eyeoff.png';
+    pass1 = 1;
+  }
+}
