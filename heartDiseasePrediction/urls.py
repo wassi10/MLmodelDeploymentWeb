978b96bd73db7e4cd317dc45306b8a885b360a3c
wassi10 <khadizawassi@gmail.com>
2024-02-11 23:14:24 +0600

    Forgot password done

--- heartDiseasePrediction/urls.py
+++ heartDiseasePrediction/urls.py
@@ -17,15 +17,26 @@ urlpatterns = [
     path('prevention', views.Prevention , name='prevention'),
     path('DoctorAndHospital', views.DoctorAndHospital , name='DoctorAndHospital'),
 
+    # pass: Hello1@23
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='reset_password'),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+
+    path('reset_password_complete>/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    # path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+
+
+    # path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     # path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
 
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    # path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
 
 
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+    # path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 
 ]
