3bb11ca8e23dfca04598722c7cc3798992a4fed7
wassi10 <khadizawassi@gmail.com>
2024-02-11 19:04:19 +0600

    forgot password

--- heartDiseasePrediction/urls.py
+++ heartDiseasePrediction/urls.py
@@ -17,12 +17,10 @@ urlpatterns = [
     path('DoctorAndHospital', views.DoctorAndHospital , name='DoctorAndHospital'),
 
 
-    path('password-reset', auth_views.PasswordChangeView.as_view(template_name='password_reset.html'), name='password-reset'),
 
-    path('password-reset-done', auth_views.PasswordChangeDoneView.as_view(template_name='password_reset_done.html'), name='password-reset-done'),
-
-    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password-reset-confirm'),
-
-    path('password-reset-complete', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html') , name='password-reset-complete'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 
 ]
