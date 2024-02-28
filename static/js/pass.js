729870f60e6976c550a5e63ddda3c88a7930eac9
wassi10 <khadizawassi@gmail.com>
2024-02-29 01:26:38 +0600

    password image changed

--- static/js/pass.js
+++ static/js/pass.js
@@ -2,12 +2,12 @@ var a
 function pass() {
     if (a == 1) {
         document.getElementById('password1').type = 'password';
-        document.getElementById('pass-icon').src = 'static/img/pass-hide.png';
+        document.getElementById('pass-icon').src = 'static/img/eyeon.png';
         a = 0;
     }
     else {
         document.getElementById('password1').type = 'text';
-        document.getElementById('pass-icon').src = 'static/img/pass-hide.png';
+        document.getElementById('pass-icon').src = 'static/img/eyeoff.png';
         a = 1;
     }
 }
--- static/js/pass1.js
+++ static/js/pass1.js
@@ -2,12 +2,12 @@ var b
 function pass1() {
     if (b == 1) {
         document.getElementById('password2').type = 'password';
-        document.getElementById('pass-icon1').src = 'static/img/pass-hide.png';
+        document.getElementById('pass-icon1').src = 'static\img\eyeon.png';
         b = 0;
     }
     else {
         document.getElementById('password2').type = 'text';
-        document.getElementById('pass-icon1').src = 'static/img/pass-hide.png';
+        document.getElementById('pass-icon1').src = 'static/img/eyeoff.png';
         b = 1;
     }
 }
