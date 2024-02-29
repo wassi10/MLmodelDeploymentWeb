3edc10c37751250d2cfbf95d51d7e023dfd8a5bf
wassi10 <khadizawassi@gmail.com>
2024-02-29 13:54:09 +0600

    responsive start

--- static/js/main.js
+++ static/js/main.js
@@ -5,31 +5,43 @@ window.addEventListener('scroll', function () {
   header.classList.toggle('sticky', window.scrollY > 0)
 })
 
-// forgot pass eyeon, off
-var pass
-function resetpass() {
-  if (pass == 1) {
-    document.getElementById('password1').type = 'password';
-    document.getElementById('pass-icon').src = 'static/img/eyeon.png';
-    pass = 0;
-  }
-  else {
-    document.getElementById('password1').type = 'text';
-    document.getElementById('pass-icon').src = 'static/img/eyeoff.png';
-    pass = 1;
-  }
-}
 
-var pass1
-function resetpass1() {
-  if (pass1 == 1) {
-    document.getElementById('password2').type = 'password';
-    document.getElementById('pass-icon1').src = 'static/img/eyeon.png';
-    pass1 = 0;
-  }
-  else {
-    document.getElementById('password2').type = 'text';
-    document.getElementById('pass-icon1').src = 'static/img/eyeoff.png';
-    pass1 = 1;
-  }
-}
+// const sr = ScrollReveal ({
+//   distance: '20px',
+//   duration: 3000,
+//   reset: true
+// });
+// sr.reveal('.header',{delay: 190, origin: 'left'});
+// sr.reveal('.home, .heading',{delay: 200, origin: 'top'});
+// sr.reveal('.about-img, .about-content, .education-row, .skills-content, .projects-content, form, .footer',{delay: 200, origin: 'bottom'});
+
+
+
+// forgot pass eyeon, off
+// var pass
+// function resetpass() {
+//   if (pass == 1) {
+//     document.getElementById('password1').type = 'password';
+//     document.getElementById('pass-icon').src = 'static/img/eyeon.png';
+//     pass = 0;
+//   }
+//   else {
+//     document.getElementById('password1').type = 'text';
+//     document.getElementById('pass-icon').src = 'static/img/eyeoff.png';
+//     pass = 1;
+//   }
+// }
+
+// var pass1
+// function resetpass1() {
+//   if (pass1 == 1) {
+//     document.getElementById('password2').type = 'password';
+//     document.getElementById('pass-icon1').src = 'static/img/eyeon.png';
+//     pass1 = 0;
+//   }
+//   else {
+//     document.getElementById('password2').type = 'text';
+//     document.getElementById('pass-icon1').src = 'static/img/eyeoff.png';
+//     pass1 = 1;
+//   }
+// }
--- static/js/pass1.js
+++ static/js/pass1.js
@@ -2,7 +2,7 @@ var b
 function pass1() {
     if (b == 1) {
         document.getElementById('password2').type = 'password';
-        document.getElementById('pass-icon1').src = 'static\img\eyeon.png';
+        document.getElementById('pass-icon1').src = 'static/img/eyeon.png';
         b = 0;
     }
     else {
